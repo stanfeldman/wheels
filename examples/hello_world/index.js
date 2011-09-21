c9834bc207bd8c317e576e8d8cd7609f4da0d7e3
Stanislav Feldman <stanislavfeldman@gmail.com>
2011-09-21 17:08:36 +0400

    comment models because of errors and add events

--- examples/hello_world/index.js
+++ examples/hello_world/index.js
@@ -6,20 +6,22 @@ var options =
 {
 	address: "127.0.0.1",
 	port: 1337,
-	mapping:
+	controllers:
 	{
 		"/$": controllers.Controller1,
 		"/2/?$": controllers.Controller2,
 		"/(\\d+).(\\d+)/?$": controllers.Controller2
 	},
-	db:
+	models:
 	{
-		adapter: kiss.models.adapters.MongodbAdapter, 
+		//adapter: kiss.models.adapters.MongodbAdapter, 
 		host: "127.0.0.1",
 		port: 27017, 
-		name: "test_db", 
-		user: "root", 
-		password: "!1ebet2@"
+		name: "test"
+	},
+	eventer:
+	{
+		"before_request": function(){ console.log("before_request!"); }
 	}
 };
 var app = new kiss.core.Application(options);
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ exports.core = require('./lib/core');
 exports.controllers = require('./lib/controllers');
 exports.views = require('./lib/views');
 exports.models = require('./lib/models');
-exports.models.adapters = require("./lib/adapters");
+//exports.models.adapters = require("./lib/adapters");
