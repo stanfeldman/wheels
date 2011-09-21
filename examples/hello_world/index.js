0a2a0e50ffd1bf618327718c4b1c7202c70a39e3
Stanislav Feldman <stanislavfeldman@gmail.com>
2011-09-21 09:58:12 +0400

    refactored Application options

--- examples/hello_world/index.js
+++ examples/hello_world/index.js
@@ -2,11 +2,25 @@ require.paths.push('/usr/lib/node_modules');
 var kiss = require("kiss.js");
 var controllers = require("./controllers");
 
-var mapping = 
+var options =
 {
-    "/$": controllers.Controller1,
-    "/2/?$": controllers.Controller2,
-    "/(\\d+).(\\d+)/?$": controllers.Controller2
+	address: "127.0.0.1",
+	port: 1337,
+	mapping:
+	{
+		"/$": controllers.Controller1,
+		"/2/?$": controllers.Controller2,
+		"/(\\d+).(\\d+)/?$": controllers.Controller2
+	},
+	db:
+	{
+		engine: "pywy.models.mongodb.MongodbAdapter", 
+		host: "127.0.0.1", 
+		port: 27017, 
+		name: "test_db", 
+		user: "root", 
+		password: "!1ebet2@"
+	}
 };
-var app = new kiss.core.Application(mapping);
+var app = new kiss.core.Application(options);
 app.start();
